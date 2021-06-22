1b203394260a237c52c62a13d7379b214e66ae33
Bunks <busk_1@yahoo.com>
2021-06-22 10:50:12 -0400

    enabled feign on main

--- FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/FistToFiveEdgeServiceApplication.java
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/FistToFiveEdgeServiceApplication.java
@@ -2,8 +2,12 @@ package com.company.FistToFiveEdgeService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class FistToFiveEdgeServiceApplication {
 
 	public static void main(String[] args) {
