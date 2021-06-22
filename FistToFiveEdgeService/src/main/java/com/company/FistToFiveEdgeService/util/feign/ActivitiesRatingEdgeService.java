dee846bf9f3e57398d4f6ab7d38eb3a018d2a949
Bunks <busk_1@yahoo.com>
2021-06-22 13:31:06 -0400

    attached feign clients to the services that will be using it

---
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ActivitiesRatingEdgeService.java
@@ -0,0 +1,10 @@
+package com.company.FistToFiveEdgeService.util.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "Activities-Ratings")
+public interface ActivitiesRatingEdgeService {
+
+    // Add routes here?
+
+}
---
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ProjectAuthEdgeService.java
@@ -0,0 +1,9 @@
+package com.company.FistToFiveEdgeService.util.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(name = "account-service")
+public interface ProjectAuthEdgeService {
+
+//    add routes here?
+}
