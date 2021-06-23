79526ea689bda458dd8dc1897c2b6ba1eab178af
Bunks <busk_1@yahoo.com>
2021-06-23 15:01:14 -0400

    minor refactor

--- FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/controller/ActivitiesController.java
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/controller/ActivitiesController.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
