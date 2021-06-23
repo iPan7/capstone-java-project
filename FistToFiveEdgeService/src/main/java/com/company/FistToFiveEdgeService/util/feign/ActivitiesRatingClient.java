65f9780c2b69d14e2a90bb2947d8e1cc2fd85d32
Bunks <busk_1@yahoo.com>
2021-06-23 15:00:48 -0400

    finished activities edge service

--- FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ActivitiesRatingClient.java
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ActivitiesRatingClient.java
@@ -1,11 +1,27 @@
 package com.company.FistToFiveEdgeService.util.feign;
 
+import com.company.FistToFiveEdgeService.models.Activity;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @FeignClient(name = "Activities-Ratings")
 public interface ActivitiesRatingClient {
 
-    @GetMapping("/rating")
-    public String getRating();
+    @GetMapping("/activity")
+    public List<Activity> getAllActivities();
+
+    @GetMapping("/activity/{id}")
+    public Activity getActivityById(@PathVariable int id);
+
+    @PostMapping("/activity")
+    public Activity createActivity(@RequestBody Activity activity);
+
+    @PutMapping("/activity/{id}")
+    public void updateActivity(@PathVariable int id, @RequestBody Activity activity);
+
+    @DeleteMapping("/activity/{id}")
+    public void deleteActivity(@PathVariable int id);
+
 }
