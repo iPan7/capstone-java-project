12afb37439fb2df5f8644f567174326f371b76fb
Hannah Starcevich <75180727+HannahStarcevich@users.noreply.github.com>
2021-06-22 20:54:23 -0700

    Delete Activities-Ratings directory

--- Activities-Ratings/src/main/java/com/company/ActivitiesRatings/controller/ActivityController.java
+++
@@ -1,4 +0,0 @@
-package com.company.ActivitiesRatings.controller;
-
-public class ActivityController {
-}
--- Activities-Ratings/src/main/java/com/company/ActivitiesRatings/repository/RatingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.ActivitiesRatings.repository;
-
-import com.company.ActivitiesRatings.model.Rating;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Integer> {
-}
