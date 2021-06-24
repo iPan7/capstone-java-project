cb8d75ea5bb9bed487c6ad7484b30e173185d441
Bunks <busk_1@yahoo.com>
2021-06-23 23:35:34 -0400

    added auth to the edge service - controller and feign client

--- FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ProjectAuthClient.java
+++ FistToFiveEdgeService/src/main/java/com/company/FistToFiveEdgeService/util/feign/ProjectAuthClient.java
@@ -4,11 +4,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 
 @FeignClient(name = "account-service")
 public interface ProjectAuthClient {
+
+    @PostMapping("/login")
+    public String getLogin(Principal principal);
 
     @GetMapping("/studentDashboard")
     public String viewStudentDashboard(Principal principal);
